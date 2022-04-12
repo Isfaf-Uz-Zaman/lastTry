6388a303e2fa0591cf87dfbb170bf2b9761fea98
Isfaf-Uz-Zaman <66315494+Isfaf-Uz-Zaman@users.noreply.github.com>
2022-04-13 01:43:27 +0600

    make it better

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pickle
+
+model = pickle.load(open('final_model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    Pregnancies = request.form.get('Pregnancies')
+    Glucose = request.form.get('Glucose')
+    BloodPressure = request.form.get('BloodPressure')
+    SkinThickness = request.form.get('SkinThickness')
+    Insulin = request.form.get('Insulin')
+    BMI = request.form.get('BMI')
+    DiabetesPedigreeFunction = request.form.get('DiabetesPedigreeFunction')
+    Age = request.form.get('Age')
+
+    input_query = np.array(
+        [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+    result = model.predict(input_query)[0]
+
+    return jsonify({'Diabetes': str(result)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
